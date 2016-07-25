bae3d030238811868a845ab131a40f3b3f2629dd
Tian Lun <tianlunlee@gmail.com>
2016-07-25 12:18:48 -0700

    preliminary

--- main.py
+++ main.py
@@ -12,17 +12,58 @@ jinja_environment = jinja2.Environment(
 
 class User(ndb.Model):
     name = ndb.StringProperty()
+    goal = ndb.FloatProperty()
+    score = ndb.IntegerProperty()
 
+    # university = ndb.StringProperty()
+    # university_key = ndb.KeyProperty(kind=University)
+class CurrentBudget(ndb.Model):
+    value = ndb.FloatProperty()
+
+class Money(ndb.Model):
+    # this model
+    source_name = ndb.StringProperty()
+    user_key = ndb.KeyProperty(kind=User)
+    balance = ndb.FloatProperty()
+
+    def decrease_value(self, amount):
+        self.balance -= amount
+
+    # def set_value(self, amount):
+    #     self.balance = amount
+
+    def add_value(self, amount):
+        self.balance += amount
+
+class PastBudget(ndb.Model):
+    source_name = ndb.StringProperty()
+    user_key = ndb.KeyProperty(kind=User)
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        template = jinja_environment.get_template('main.html')
-        self.response.write('Hello world!')
+        user = users.get_current_user()
+
+
+        if user:
+            logout_url = users.CreateLogoutURL('/')
+
+            template = jinja_environment.get_template('main.html')
+            template_vals = {'user':user, 'log_url':log_url}
+            self.response.write(template.render(template_vals))
+
+        else:
+            login_url = users.CreateLoginURL('/')
+
+            self.response.write('Logged Out')
 
 
 
 
+
+
+    def post(self):
+        self.redirect('/')
 
 
 
